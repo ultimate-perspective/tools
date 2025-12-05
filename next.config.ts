c2256f7e5d7e994b5be587f34390e82e52e99800
Dhruvil Shah <designinstantly@gmail.com>
2025-12-05 15:45:13 +0530

    feat: added base path in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/free-tools",
+  assetPrefix: "/free-tools"
 };
 
 export default nextConfig;
