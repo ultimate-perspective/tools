2b5f883e5d610a18b147ebf013ae6065a6e6bfb8
Dhruvil Shah <designinstantly@gmail.com>
2025-12-13 10:20:05 +0530

    feat: updated sitemap and robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: "*",
             allow: "/",
         },
-        sitemap: "https://designinstantly.com/sitemap.xml",
+        sitemap: "https://designinstantly.com/free-tools/sitemap.xml",
     };
 }
